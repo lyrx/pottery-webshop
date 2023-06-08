c6a4d23848bb86d7a14f1edcd21fa13367103628
alex <enna@mailbox.org>
2023-06-08 17:09:44 +0200

    One functional component for each section from the template

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import ShopHeader from "./components/ShopHeader";
+import Banner from "./components/Banner";
+import SectionOne from "./components/SectionOne";
+import SectionTwo from "./components/SectionTwo";
+import SectionThree from "./components/SectionThree";
+import SectionFour from "./components/SectionFour";
+import SectionFive from "./components/SectionFive";
 
 function App() {
-  return (
-    <div className="App">
-     <ShopHeader />
-    </div>
-  );
+    return (
+        <div className="App">
+            <ShopHeader/>
+            <Banner/>
+            <SectionOne/>
+            <SectionTwo/>
+            <SectionThree />
+            <SectionFour />
+            <SectionFive />
+        </div>
+    );
 }
 
 export default App;
